4e605cce94b741a5322ecf77e1c01c3804d5d902
1 <1@172.31.3.219>
2018-04-18 19:47:13 +0300

    Fix Controllers

--- e-Shop/src/main/java/org/itstep/controller/CartController.java
+++ e-Shop/src/main/java/org/itstep/controller/CartController.java
@@ -1,5 +1,7 @@
 package org.itstep.controller;
 
+import java.util.List;
+
 import org.itstep.model.Cart;
 import org.itstep.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,31 +9,59 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping (path = "/cart")
+@RequestMapping(path = "/cart")
 public class CartController {
 
 	@Autowired
 	CartService cartService;
-	
-	@PostMapping ( consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	ResponseEntity<Cart> saveCart(@RequestParam Cart cart){
-		if(cartService.save(cart) != null) {
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	ResponseEntity<Cart> saveCart(@RequestBody Cart cart) {
+		if (cartService.save(cart) != null) {
 			return new ResponseEntity<Cart>(cart, HttpStatus.OK);
 		}
 		return new ResponseEntity(HttpStatus.BAD_REQUEST);
 	}
-	
-	@PutMapping ( consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	ResponseEntity<Cart> updateCart(@RequestParam Cart cart){
-		if(cartService.update(cart) != null) {
+
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	ResponseEntity<Cart> updateCart(@RequestBody Cart cart) {
+		if (cartService.update(cart) != null) {
 			return new ResponseEntity<Cart>(cart, HttpStatus.OK);
 		}
 		return new ResponseEntity(HttpStatus.BAD_REQUEST);
 	}
+
+	@DeleteMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	ResponseEntity<Cart> deleteCart(@RequestBody Cart cart) {
+		cartService.delete(cart);
+		return new ResponseEntity(HttpStatus.OK);
+	}
+
+	@GetMapping(path = "/get-one", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	ResponseEntity<Cart> getCart(@RequestBody Integer id) {
+		Cart cartFromDB = cartService.get(id);
+		if (cartFromDB != null) {
+			return new ResponseEntity<Cart>(cartFromDB, HttpStatus.OK);
+		}
+		return new ResponseEntity(HttpStatus.BAD_REQUEST);
+	}
+
+	@GetMapping(path = "/get-list", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	ResponseEntity<List<Cart>> getCartsByPeriod(@RequestBody Long startPeriod, @RequestBody Long endPeriod,
+			@RequestBody String login) {
+		List<Cart> cartsFromDB = cartService.findAllByCreationTime(startPeriod, endPeriod, login);
+		if (cartsFromDB != null) {
+			return new ResponseEntity<List<Cart>>(cartsFromDB, HttpStatus.OK);
+		}
+		return new ResponseEntity(HttpStatus.BAD_REQUEST);
+	}
+
 }
--- e-Shop/src/main/java/org/itstep/controller/OrderController.java
+++ e-Shop/src/main/java/org/itstep/controller/OrderController.java
@@ -1,16 +1,19 @@
 package org.itstep.controller;
 
 import org.itstep.model.Order;
+import org.itstep.model.Order;
 import org.itstep.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(path = "/order")
@@ -18,25 +21,38 @@ public class OrderController {
 
 	@Autowired
 	OrderService orderService;
-	
+
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	ResponseEntity<Order> saveOrder(@RequestParam Order order){
-		
+	ResponseEntity<Order> saveOrder(@RequestBody Order order) {
+
 		if (orderService.save(order) != null) {
-			return new ResponseEntity(HttpStatus.OK);
+			return new ResponseEntity<Order>(order, HttpStatus.OK);
 		}
-		
 		return new ResponseEntity(HttpStatus.BAD_REQUEST);
 	}
-	
-	
+
 	@PutMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	ResponseEntity<Order> updateOrder(@RequestParam Order order){
-		
+	ResponseEntity<Order> updateOrder(@RequestBody Order order) {
+
 		if (orderService.save(order) != null) {
-			return new ResponseEntity(HttpStatus.OK);
+			return new ResponseEntity<Order>(order, HttpStatus.OK);
 		}
-		
+
+		return new ResponseEntity(HttpStatus.BAD_REQUEST);
+	}
+
+	@DeleteMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	ResponseEntity<Order> deleteOrder(@RequestBody Order order) {
+		orderService.delete(order);
+		return new ResponseEntity(HttpStatus.OK);
+	}
+
+	@GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	ResponseEntity<Order> getOrder(@RequestBody Integer id) {
+		Order orderFromDB = orderService.get(id);
+		if (orderFromDB != null) {
+			return new ResponseEntity<Order>(orderFromDB, HttpStatus.OK);
+		}
 		return new ResponseEntity(HttpStatus.BAD_REQUEST);
 	}
 }
